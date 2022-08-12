61f3c7efb77a0ed1f8167bca7de25ac2c1ebfacd
Ruan van der Merwe <ruan@colab.in.na>
2022-08-12 19:43:58 +0200

    Task 5 complete

--- task5.php
+++ task5.php
@@ -16,19 +16,21 @@ $actors = array();
 $role_types = array();
 $film_titles = array();
 $film_actor_roles = array();
+$radio_query = array();
 
 
 function fetchAll()
 {
     try {
 
-        $GLOBALS['actors'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.actors");
+        $GLOBALS['actors'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.actors ORDER BY ActorID");
 
 
-        $GLOBALS['role_types'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.roletypes");
+        $GLOBALS['role_types'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.roletypes ORDER BY RoleTypeID");
 
 
-        $GLOBALS['film_titles'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.filmtitles");
+        $GLOBALS['film_titles'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.filmtitles ORDER BY FilmTitleID");
+
         $GLOBALS['film_actor_roles'] = $GLOBALS['conn']->query("SELECT far.FilmTitleID,
                                                                         ft.FilmTitle,
                                                                         ft.FilmDuration,
@@ -43,7 +45,51 @@ function fetchAll()
                                                                 LEFT JOIN ass3task5.filmtitles ft on far.FilmTitleID = ft.FilmTitleID
                                                                 LEFT JOIN ass3task5.roletypes rt on far.RoleTypeID = rt.RoleTypeID
                                                                 LEFT JOIN ass3task5.actors a on far.ActorID = a.ActorID
-                                                                ");
+                                                                ORDER BY far.FilmTitleID");
+
+        if (isset($_POST['query'])) {
+            $query_choice = $_POST['query'];
+            if ($query_choice == "select_1") {
+                $GLOBALS['radio_query'] = $GLOBALS['conn']->query("SELECT far.FilmTitleID,
+                                                                        far.ActorID,
+                                                                        far.RoleTypeID,                                                                        
+                                                                        far.CharacterName, 
+                                                                        far.CharacterDescription 
+                                                                FROM ass3task5.filmactorroles far 
+                                                                ORDER BY CharacterName");
+            }
+            if ($query_choice == "select_2") {
+                $GLOBALS['radio_query'] = $GLOBALS['conn']->query("SELECT far.FilmTitleID,
+                                                                        far.ActorID,
+                                                                        far.RoleTypeID,                                                                        
+                                                                        far.CharacterName, 
+                                                                        far.CharacterDescription 
+                                                                FROM ass3task5.filmactorroles far 
+                                                                WHERE far.CharacterName LIKE '%ac%' ");
+            }
+            if ($query_choice == "select_3") {
+                $GLOBALS['radio_query'] = $GLOBALS['conn']->query("SELECT far.FilmTitleID,
+                                                                        far.ActorID,
+                                                                        far.RoleTypeID,                                                                        
+                                                                        far.CharacterName, 
+                                                                        far.CharacterDescription,
+                                                                        rt.RoleType
+                                                                FROM ass3task5.filmactorroles far 
+                                                                INNER JOIN ass3task5.roletypes rt ON rt.RoleTypeID = far.RoleTypeID");
+            }
+            if ($query_choice == "select_4") {
+                $GLOBALS['radio_query'] = $GLOBALS['conn']->query("SELECT far.FilmTitleID,
+                                                                        far.ActorID,
+                                                                        far.RoleTypeID,                                                                        
+                                                                        far.CharacterName, 
+                                                                        far.CharacterDescription
+                                                                FROM ass3task5.filmactorroles far 
+                                                                WHERE far.RoleTypeID = 3 OR far.RoleTypeID = 5");
+            }
+            if ($query_choice == "select_5") {
+                $GLOBALS['radio_query'] = $GLOBALS['conn']->query("SELECT MAX(ft.FilmDuration) AS FilmDuration FROM ass3task5.filmtitles ft ");
+            }
+        }
 
     } catch (PDOException $e) {
         $GLOBALS['has_error'] = true;
@@ -65,8 +111,8 @@ fetchAll();
     }
 
     .row {
-        margin-left:-5px;
-        margin-right:-5px;
+        margin-left: -5px;
+        margin-right: -5px;
     }
 
     .column {
@@ -114,7 +160,7 @@ fetchAll();
         <div class="row">
             <div class="column">
                 <h2>Actors</h2>
-                <table id="dataTables">
+                <table>
                     <thead>
                     <tr>
                         <th style="text-align: left">ActorID</th>
@@ -137,7 +183,7 @@ fetchAll();
             </div>
             <div class="column">
                 <h2>Role Types</h2>
-                <table id="dataTables">
+                <table>
                     <thead>
                     <tr>
                         <th style="text-align: left">RoleTypeID</th>
@@ -160,7 +206,7 @@ fetchAll();
             </div>
             <div class="column">
                 <h2>FilmTitles</h2>
-                <table id="dataTables">
+                <table>
                     <thead>
                     <tr>
                         <th style="text-align: left">FilmTitleID</th>
@@ -184,43 +230,211 @@ fetchAll();
                 </table>
             </div>
         </div>
+        <div>
+            <h2>FilmActorRoles (With JOINED values)</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th style="text-align: left">FilmTitleID</th>
+                    <th style="text-align: left">FilmTitle</th>
+                    <th style="text-align: left">FilmDuration</th>
+                    <th style="text-align: left">FilmStory</th>
+                    <th style="text-align: left">ActorID</th>
+                    <th style="text-align: left">ActorFullName</th>
+                    <th style="text-align: left">RoleTypeID</th>
+                    <th style="text-align: left">RoleType</th>
+                    <th style="text-align: left">CharacterName</th>
+                    <th style="text-align: left">CharacterDescription</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($film_actor_roles as $film_actor_role) { ?>
+                    <tr>
+                        <td><?php echo $film_actor_role['FilmTitleID']; ?></td>
+                        <td><?php echo $film_actor_role['FilmTitle'] ?></td>
+                        <td><?php echo $film_actor_role['FilmDuration']; ?></td>
+                        <td><?php echo $film_actor_role['FilmStory']; ?></td>
+                        <td><?php echo $film_actor_role['ActorID']; ?></td>
+                        <td><?php echo $film_actor_role['ActorFullName']; ?></td>
+                        <td><?php echo $film_actor_role['RoleTypeID']; ?></td>
+                        <td><?php echo $film_actor_role['RoleType']; ?></td>
+                        <td><?php echo $film_actor_role['CharacterName']; ?></td>
+                        <td><?php echo $film_actor_role['CharacterDescription']; ?></td>
+                    </tr>
+
+                <?php } ?>
+
+                </tbody>
+            </table>
+        </div>
+        <div>
+            <h2>Radio Button Queries</h2>
+            <?php if (isset($_POST['query'])): ?>
+                <?php if ($_POST['query'] == "select_1"): ?>
+                    <h4>Displaying Query 1</h4>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th style="text-align: left">FilmTitleID</th>
+                            <th style="text-align: left">ActorID</th>
+                            <th style="text-align: left">RoleTypeID</th>
+                            <th style="text-align: left">CharacterName</th>
+                            <th style="text-align: left">CharacterDescription</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($radio_query as $row) { ?>
+                            <tr>
+                                <td><?php echo $row['FilmTitleID']; ?></td>
+                                <td><?php echo $row['ActorID']; ?></td>
+                                <td><?php echo $row['RoleTypeID']; ?></td>
+                                <td><?php echo $row['CharacterName']; ?></td>
+                                <td><?php echo $row['CharacterDescription']; ?></td>
+                            </tr>
+
+                        <?php } ?>
+
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+                <?php if ($_POST['query'] == "select_2"): ?>
+                    <h4>Displaying Query 2</h4>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th style="text-align: left">FilmTitleID</th>
+                            <th style="text-align: left">ActorID</th>
+                            <th style="text-align: left">RoleTypeID</th>
+                            <th style="text-align: left">CharacterName</th>
+                            <th style="text-align: left">CharacterDescription</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($radio_query as $row) { ?>
+                            <tr>
+                                <td><?php echo $row['FilmTitleID']; ?></td>
+                                <td><?php echo $row['ActorID']; ?></td>
+                                <td><?php echo $row['RoleTypeID']; ?></td>
+                                <td><?php echo $row['CharacterName']; ?></td>
+                                <td><?php echo $row['CharacterDescription']; ?></td>
+                            </tr>
+
+                        <?php } ?>
+
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+                <?php if ($_POST['query'] == "select_3"): ?>
+                    <h4>Displaying Query 3</h4>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th style="text-align: left">FilmTitleID</th>
+                            <th style="text-align: left">ActorID</th>
+                            <th style="text-align: left">RoleTypeID</th>
+                            <th style="text-align: left">CharacterName</th>
+                            <th style="text-align: left">CharacterDescription</th>
+                            <th style="text-align: left">RoleType</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($radio_query as $row) { ?>
+                            <tr>
+                                <td><?php echo $row['FilmTitleID']; ?></td>
+                                <td><?php echo $row['ActorID']; ?></td>
+                                <td><?php echo $row['RoleTypeID']; ?></td>
+                                <td><?php echo $row['CharacterName']; ?></td>
+                                <td><?php echo $row['CharacterDescription']; ?></td>
+                                <td><?php echo $row['RoleType']; ?></td>
+                            </tr>
+
+                        <?php } ?>
+
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+                <?php if ($_POST['query'] == "select_4"): ?>
+                    <h4>Displaying Query 4</h4>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th style="text-align: left">FilmTitleID</th>
+                            <th style="text-align: left">ActorID</th>
+                            <th style="text-align: left">RoleTypeID</th>
+                            <th style="text-align: left">CharacterName</th>
+                            <th style="text-align: left">CharacterDescription</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($radio_query as $row) { ?>
+                            <tr>
+                                <td><?php echo $row['FilmTitleID']; ?></td>
+                                <td><?php echo $row['ActorID']; ?></td>
+                                <td><?php echo $row['RoleTypeID']; ?></td>
+                                <td><?php echo $row['CharacterName']; ?></td>
+                                <td><?php echo $row['CharacterDescription']; ?></td>
+                            </tr>
+
+                        <?php } ?>
+
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+                <?php if ($_POST['query'] == "select_5"): ?>
+                    <h4>Displaying Query 5</h4>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th style="text-align: left">FilmDuration</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($radio_query as $row) { ?>
+                            <tr>
+                                <td><?php echo $row['FilmDuration']; ?></td>
+                            </tr>
+
+                        <?php } ?>
+
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+            <?php endif; ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+                <div>
+                    <label for="select_1">
+                        <input type="radio" name="query" id="select_1" value="select_1"/>
+                        ORDER BY CharacterName
+                    </label>
+                    <br>
+                    <label for="select_2">
+                        <input type="radio" name="query" id="select_2" value="select_2"/>
+                        CharacterName LIKE '%ac%'
+                    </label>
+                    <br>
+                    <label for="select_3">
+                        <input type="radio" name="query" id="select_3" value="select_3"/>
+                        INNER JOIN RoleType
+                    </label>
+                    <br>
+                    <label for="select_4">
+                        <input type="radio" name="query" id="select_4" value="select_4"/>
+                        WHERE RoleTypeID = 3 OR RoleTypeID = 5
+                    </label>
+                    <br>
+                    <label for="select_5">
+                        <input type="radio" name="query" id="select_5" value="select_5"/>
+                        SELECT MAX(FilmDuration)
+                    </label>
+                </div>
+
+                <input type="submit" value="Submit"><br>
+            </form>
+        </div>
 
     <?php endif; ?>
 
-    <!--    <form action="--><?php //echo $_SERVER['PHP_SELF']; ?><!--" method="POST">-->
-    <!--        <div id="data">-->
-    <!--            <div>-->
-    <!--                <label for="method">Method</label>-->
-    <!--                <select name="method" id="method">-->
-    <!--                    <option value="add">Add</option>-->
-    <!--                    <option value="update">Update</option>-->
-    <!--                    <option value="delete">Delete</option>-->
-    <!--                </select>-->
-    <!--            </div>-->
-    <!--            <div>-->
-    <!--                <label for="practicenumber">Practice Number</label>-->
-    <!--                <input type="number" id="practicenumber" name="practicenumber">-->
-    <!---->
-    <!--                <label for="name">Name</label>-->
-    <!--                <input type="text" id="name" name="name">-->
-    <!---->
-    <!--                <label for="specialty">Specialty</label>-->
-    <!--                <select id="specialty" name="specialty">-->
-    <!--                    <option value="GP">GP</option>-->
-    <!--                    <option value="Oncologist">Oncologist</option>-->
-    <!--                    <option value="Surgeon">Surgeon</option>-->
-    <!--                </select>-->
-    <!--                <label for="fee">Fee</label>-->
-    <!--                <input type="number" step=".01" id="fee" name="fee">-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--        <br>-->
-    <!--        <br>-->
-    <!--        <div id="buttons">-->
-    <!--            <label>&nbsp;</label>-->
-    <!--            <input type="submit" value="Submit"><br>-->
-    <!--        </div>-->
-    <!--    </form>-->
 
     <iframe src="task5.txt" height="500" width="1500">
         Your browser does not support iframes.
