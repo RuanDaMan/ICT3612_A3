0c07ec652532b89ba5e58de6269ce5df4c8fe272
Ruan van der Merwe <ruan@colab.in.na>
2022-08-12 19:04:47 +0200

    Progress with task 5

--- task5.php
+++ task5.php
@@ -2,133 +2,48 @@
 
 
 $servername = "localhost";
-$username = "id17186169_root";
+$username = "root";
 //$username = "root";
-$password = "k&V>~Ld0Kvfz?|nZ";
+$password = "";
 //$password = "";
-$conn = new PDO("mysql:host=$servername;dbname=id17186169_ass3task4", $username, $password);
+$conn = new PDO("mysql:host=$servername;dbname=ass3task5", $username, $password);
 //$conn = new PDO("mysql:host=$servername;dbname=ass3task4", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $has_error = false;
 $error_msg = "";
 
-$doctors = array();
-$show_fee = false;
+$actors = array();
+$role_types = array();
+$film_titles = array();
+$film_actor_roles = array();
 
-
-
-if (isset($_POST["method"])) {
-
-    if ($_POST["method"] == "add") {
-        $test = "add";
-        Add();
-    }
-    if ($_POST["method"] == "update") {
-        $test = "update";
-        Update();
-    }
-    if ($_POST["method"] == "delete") {
-        $test = "delete";
-        Delete();
-    }
-} else {
-    fetchAll();
-}
-
-function Add()
-{
-    try {
-        if (empty($_POST["practicenumber"]) || empty($_POST["name"]) || empty($_POST["specialty"]) || empty($_POST["fee"])) {
-            $GLOBALS['has_error'] = true;
-            $GLOBALS['error_msg'] = "Form Invalid";
-            return;
-        }
-
-        $GLOBALS['conn']->query("INSERT INTO `doctors`(`practicenumber`, `name`, `specialty`, `fee`) VALUES ({$_POST["practicenumber"]},'{$_POST["name"]}','{$_POST["specialty"]}',{$_POST["fee"]})");
-        fetchAll();
-
-    } catch (PDOException $e) {
-        $GLOBALS['has_error'] = true;
-        $GLOBALS['error_msg'] = "Connection failed: " . $e->getMessage();
-        fetchAll();
-    }
-}
-
-function Update()
-{
-    try {
-        if (empty($_POST["practicenumber"])) {
-            $GLOBALS['has_error'] = true;
-            $GLOBALS['error_msg'] = "Form Invalid";
-            return;
-        }
-        $sql = "UPDATE `doctors` SET ";
-        $added = false;
-
-        if (isset($_POST["name"]) && $_POST["name"] != "") {
-            $sql .= " `name` = '{$_POST["name"]}' ";
-            $added = true;
-        }
-
-        if (isset($_POST["specialty"]) && $_POST["specialty"] != "") {
-            if ($added) {
-                $sql .= ", `specialty` = '{$_POST["specialty"]}' ";
-            } else {
-                $sql .= " `specialty` = '{$_POST["specialty"]}' ";
-                $added = true;
-            }
-
-        }
-
-        if (isset($_POST["fee"]) && $_POST["fee"] != "") {
-            if ($added) {
-                $sql .= ", `fee` = {$_POST["fee"]} ";
-            } else {
-                $sql .= " `fee` = {$_POST["fee"]} ";
-            }
-        }
-
-        $sql .= " WHERE practicenumber = {$_POST["practicenumber"]}";
-        if (!$added) {
-            $GLOBALS['has_error'] = true;
-            $GLOBALS['error_msg'] = "No Fields entered to update.";
-        }
-        $GLOBALS['test'] = $sql;
-        $GLOBALS['conn']->query($sql);
-        fetchAll();
-
-    } catch (PDOException $e) {
-        $GLOBALS['has_error'] = true;
-        $GLOBALS['error_msg'] = "Connection failed: " . $e->getMessage();
-        fetchAll();
-    }
-}
-
-function Delete()
-{
-    try {
-        if (empty($_POST["practicenumber"])) {
-            $GLOBALS['has_error'] = true;
-            $GLOBALS['error_msg'] = "Form Invalid";
-            return;
-        }
-
-        $GLOBALS['conn']->query("DELETE FROM `doctors` WHERE `practicenumber` = {$_POST["practicenumber"]}");
-        fetchAll();
-
-    } catch (PDOException $e) {
-        $GLOBALS['has_error'] = true;
-        $GLOBALS['error_msg'] = "Connection failed: " . $e->getMessage();
-        fetchAll();
-    }
-}
 
 function fetchAll()
 {
     try {
 
+        $GLOBALS['actors'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.actors");
 
-        $GLOBALS['doctors'] = $GLOBALS['conn']->query("SELECT * FROM doctors");
+
+        $GLOBALS['role_types'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.roletypes");
+
+
+        $GLOBALS['film_titles'] = $GLOBALS['conn']->query("SELECT * FROM ass3task5.filmtitles");
+        $GLOBALS['film_actor_roles'] = $GLOBALS['conn']->query("SELECT far.FilmTitleID,
+                                                                        ft.FilmTitle,
+                                                                        ft.FilmDuration,
+                                                                        ft.FilmStory,
+                                                                        far.ActorID,
+                                                                        a.ActorFullName,
+                                                                        far.RoleTypeID, 
+                                                                        rt.RoleType,                                                                        
+                                                                        far.CharacterName, 
+                                                                        far.CharacterDescription 
+                                                                FROM ass3task5.filmactorroles far 
+                                                                LEFT JOIN ass3task5.filmtitles ft on far.FilmTitleID = ft.FilmTitleID
+                                                                LEFT JOIN ass3task5.roletypes rt on far.RoleTypeID = rt.RoleTypeID
+                                                                LEFT JOIN ass3task5.actors a on far.ActorID = a.ActorID
+                                                                ");
 
     } catch (PDOException $e) {
         $GLOBALS['has_error'] = true;
@@ -136,84 +51,180 @@ function fetchAll()
     }
 }
 
+fetchAll();
+
+
 ?>
 
 
-<html>
+<html lang="en">
+<style>
+
+    * {
+        box-sizing: border-box;
+    }
+
+    .row {
+        margin-left:-5px;
+        margin-right:-5px;
+    }
+
+    .column {
+        float: left;
+        width: 50%;
+        padding: 5px;
+    }
+
+    /* Clearfix (clear floats) */
+    .row::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+        width: 100%;
+        border: 1px solid #ddd;
+    }
+
+    th, td {
+        text-align: left;
+        padding: 5px;
+        border: 1px solid #ddd;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+</style>
 <body>
 <?php include 'menu.inc'; ?>
 <main>
     <h1>Task 5</h1>
 
 
-    <? if ($has_error): ?>
+    <?php if ($has_error): ?>
         <p><?php echo $error_msg ?></p>
-    <? else: ?>
+    <?php else: ?>
 
         <?php ?>
-        <table>
-            <thead>
-            <tr>
-                <th>Practice Number</th>
-                <th>Name</th>
-                <th>Specialty</th>
-                <th>Fee</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($doctors as $doctor) { ?>
-                <tr>
-                    <td><?php echo $doctor['practicenumber']; ?></td>
-                    <td><?php echo $doctor['name'] ?></td>
-                    <td><?php echo $doctor['specialty']; ?></td>
-                    <td><?php echo $doctor['fee']; ?></td>
-                </tr>
+        <div class="row">
+            <div class="column">
+                <h2>Actors</h2>
+                <table id="dataTables">
+                    <thead>
+                    <tr>
+                        <th style="text-align: left">ActorID</th>
+                        <th style="text-align: left">ActorFullName</th>
+                        <th style="text-align: left">ActorNotes</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($actors as $actor) { ?>
+                        <tr>
+                            <td><?php echo $actor['ActorID']; ?></td>
+                            <td><?php echo $actor['ActorFullName'] ?></td>
+                            <td><?php echo $actor['ActorNotes']; ?></td>
+                        </tr>
 
-            <?php } ?>
+                    <?php } ?>
 
-            </tbody>
-        </table>
-        <br>
-        <br>
-
-    <? endif; ?>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <div id="data">
-            <div>
-                <label for="method">Method</label>
-                <select name="method" id="method">
-                    <option value="add">Add</option>
-                    <option value="update">Update</option>
-                    <option value="delete">Delete</option>
-                </select>
+                    </tbody>
+                </table>
             </div>
-            <div>
-                <label for="practicenumber">Practice Number</label>
-                <input type="number" id="practicenumber" name="practicenumber">
+            <div class="column">
+                <h2>Role Types</h2>
+                <table id="dataTables">
+                    <thead>
+                    <tr>
+                        <th style="text-align: left">RoleTypeID</th>
+                        <th style="text-align: left">RoleType</th>
 
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name">
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($role_types as $role_type) { ?>
+                        <tr>
+                            <td><?php echo $role_type['RoleTypeID']; ?></td>
+                            <td><?php echo $role_type['RoleType'] ?></td>
 
-                <label for="specialty">Specialty</label>
-                <select id="specialty" name="specialty">
-                    <option value="GP">GP</option>
-                    <option value="Oncologist">Oncologist</option>
-                    <option value="Surgeon">Surgeon</option>
-                </select>
-                <label for="fee">Fee</label>
-                <input type="number" step=".01" id="fee" name="fee">
+                        </tr>
+
+                    <?php } ?>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="column">
+                <h2>FilmTitles</h2>
+                <table id="dataTables">
+                    <thead>
+                    <tr>
+                        <th style="text-align: left">FilmTitleID</th>
+                        <th style="text-align: left">FilmTitle</th>
+                        <th style="text-align: left">FilmDuration</th>
+                        <th style="text-align: left">FilmStory</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($film_titles as $film_title) { ?>
+                        <tr>
+                            <td><?php echo $film_title['FilmTitleID']; ?></td>
+                            <td><?php echo $film_title['FilmTitle'] ?></td>
+                            <td><?php echo $film_title['FilmDuration']; ?></td>
+                            <td><?php echo $film_title['FilmStory']; ?></td>
+                        </tr>
+
+                    <?php } ?>
+
+                    </tbody>
+                </table>
             </div>
         </div>
-        <br>
-        <br>
-        <div id="buttons">
-            <label>&nbsp;</label>
-            <input type="submit" value="Submit"><br>
-        </div>
-    </form>
+
+    <?php endif; ?>
+
+    <!--    <form action="--><?php //echo $_SERVER['PHP_SELF']; ?><!--" method="POST">-->
+    <!--        <div id="data">-->
+    <!--            <div>-->
+    <!--                <label for="method">Method</label>-->
+    <!--                <select name="method" id="method">-->
+    <!--                    <option value="add">Add</option>-->
+    <!--                    <option value="update">Update</option>-->
+    <!--                    <option value="delete">Delete</option>-->
+    <!--                </select>-->
+    <!--            </div>-->
+    <!--            <div>-->
+    <!--                <label for="practicenumber">Practice Number</label>-->
+    <!--                <input type="number" id="practicenumber" name="practicenumber">-->
+    <!---->
+    <!--                <label for="name">Name</label>-->
+    <!--                <input type="text" id="name" name="name">-->
+    <!---->
+    <!--                <label for="specialty">Specialty</label>-->
+    <!--                <select id="specialty" name="specialty">-->
+    <!--                    <option value="GP">GP</option>-->
+    <!--                    <option value="Oncologist">Oncologist</option>-->
+    <!--                    <option value="Surgeon">Surgeon</option>-->
+    <!--                </select>-->
+    <!--                <label for="fee">Fee</label>-->
+    <!--                <input type="number" step=".01" id="fee" name="fee">-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--        <br>-->
+    <!--        <br>-->
+    <!--        <div id="buttons">-->
+    <!--            <label>&nbsp;</label>-->
+    <!--            <input type="submit" value="Submit"><br>-->
+    <!--        </div>-->
+    <!--    </form>-->
 
     <iframe src="task5.txt" height="500" width="1500">
-        Your browser does not support iframes. </iframe>
+        Your browser does not support iframes.
+    </iframe>
 </main>
 
 </body>
